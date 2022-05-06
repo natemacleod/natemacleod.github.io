360c03fa510908e54e22bf4a87d0f96bef8124d6
natemacleod <virtualnatem+gh@gmail.com>
2022-05-06 16:51:54 -0400

    finish dropdown

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,9 +1,14 @@
+// Handles the dropdown menu in the navigation bar
+
 function toggleMenu() {
     document.getElementById("topContent").classList.toggle("show");
+    if (document.getElementById("topDChevron").src.includes("down")) {
+        document.getElementById("topDChevron").src ="./icons/chevron-up.svg";
+    } else document.getElementById("topDChevron").src ="./icons/chevron-down.svg";
 }
 
 window.onclick = function(event) {
-    if (event.target.id === ("topDropdown")) {
+    if (event.target.id === ("topDropdown") || event.target.id === ("topDChevron")) {
         toggleMenu();
     } else if (document.getElementById("topContent").classList.contains("show")) {
         document.getElementById("topContent").classList.remove("show");
