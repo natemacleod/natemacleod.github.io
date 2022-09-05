401e0e00097b398732f8c1c54026ff125df46720
Nate MacLeod <virtualnatem+gh@gmail.com>
2022-09-05 17:04:00 -0400

    the penultimate update

--- js/typewriter.js
+++ js/typewriter.js
@@ -3,30 +3,30 @@ function sleep(ms) {
 }
 
 const type = async function (ftxt, ctxt, el) {
-    ctxt = ftxt.substring(0, ctxt.length + 1)
-    if (ctxt[ctxt.length - 1] === '\n') {
-        ctxt = ctxt.substring(0, ctxt.length - 1) + "<br>"
-        ftxt = "Hi.  <br>I'm Nate MacLeod."
+    while (ctxt !== ftxt) {
+        ctxt = ftxt.substring(0, ctxt.length + 1)
+        if (ctxt[ctxt.length - 1] === '\n') {
+            ctxt = ctxt.substring(0, ctxt.length - 1) + "<br>"
+            ftxt = "Hi.  <br>I'm Nate MacLeod."
+        }
+        el.innerHTML = '<span class="wrap">' + ctxt + '</span>'
+
+        var waitTime = 250 - Math.random() * 100
+
+        if (ctxt !== ftxt) {
+            await sleep(waitTime)
+        } else return "Completed typing"
     }
-    el.innerHTML = '<span class="wrap">' + ctxt + '</span>'
-
-    var delta = 250 - Math.random() * 110
-
-    if (ctxt !== ftxt) {
-        await sleep(delta)
-        return type(ftxt, ctxt, el)
-    } else return "Completed typing"
 }
 
 window.onload = async function () {
-    // INJECT CSS
     var css = document.createElement("style")
     css.innerHTML = "#tr > .wrap { border-right: 0.08em solid #dedede }"
     document.body.appendChild(css)
 
     var element = document.getElementById('tr')
     await sleep(400)
-    console.log(await type("Hi.  \nI'm Nate MacLeod.", "", element))
+    await type("Hi.  \nI'm Nate MacLeod.", "", element)
     await sleep(500)
     css.innerHTML = ""
     for (let i = 0; i < 4; i++) {
@@ -35,4 +35,5 @@ window.onload = async function () {
         await sleep(500)
         css.innerHTML = ""
     }
+    css.remove()
 }
