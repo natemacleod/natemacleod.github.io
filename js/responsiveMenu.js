db1e7c34ecf6d6463f13a65cafa4a6750ab72e8b
Nate MacLeod <virtualnatem+gh@gmail.com>
2022-09-26 10:15:02 -0400

    bug fix

--- js/responsiveMenu.js
+++ js/responsiveMenu.js
@@ -4,18 +4,18 @@ const toggle = document.getElementById("hm"); // The hamburger menu button
 const toggleImg = document.querySelector("#hm img") // The hamburger menu icon
 const menu = document.getElementById("mobileMenu"); // The mobile menu
 const links = document.querySelectorAll("#mobileMenu .link"); // An array containing all four mobile menu links
- 
+
 // Toggles the mobile menu and changes the icon
 function toggleMenu() {
     if (menu.classList.contains("inactive")) {
         menu.classList.remove("inactive");
         toggleImg.src = "./files/icons/x-lg.svg";
-    } else  {
+    } else {
         menu.classList.add("inactive");
         toggleImg.src = "./files/icons/list.svg";
     }
 }
- 
+
 // Clicking on the menu button should toggle the menu
 toggle.addEventListener("click", toggleMenu, false);
 // Clicking on a link in the menu should close the menu
